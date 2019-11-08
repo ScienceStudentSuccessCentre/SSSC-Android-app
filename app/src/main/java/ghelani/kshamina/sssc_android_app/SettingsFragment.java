8f588d6ff1e4e3a47de47409b5e447f0e972e2ea
tobiasschlagenhaufer <toby.schlagenhaufer@gmail.com>
2019-11-08 13:25:17 -0500

    added settings tab

---
+++ app/src/main/java/ghelani/kshamina/sssc_android_app/SettingsFragment.java
@@ -0,0 +1,22 @@
+package ghelani.kshamina.sssc_android_app;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup settingsView = (ViewGroup) inflater.inflate(R.layout.fragment_settings, container,false);
+        return settingsView;
+    }
+}
+
+
+
+
