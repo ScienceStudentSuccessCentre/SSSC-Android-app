91aa5aea9efb1a7f51db504eb5ec97d5a6a57ed5
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2020-06-18 08:32:08 -0400

    Remove unused ListClickListener

--- app/src/main/java/ghelani/kshamina/sssc_android_app/ui/grades/terms/add_term/ListClickListener.java
+++
@@ -1,8 +0,0 @@
-package ghelani.kshamina.sssc_android_app.ui.grades.terms.add_term;
-
-public interface ListClickListener {
-    int YEAR = 0;
-    int SEASON = 1;
-
-    void onItemSelected(int position, int type);
-}
